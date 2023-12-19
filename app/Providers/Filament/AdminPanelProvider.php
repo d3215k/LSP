181fd6d103d2cd7c841909bfecdb30d4ed6cb797
Dede Iskandar <d3215k@gmail.com>
2023-12-20 05:22:43 +0700

    wip

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('app')
             ->path('app')
+            ->spa()
             ->registration()
             ->login()
             ->colors([
