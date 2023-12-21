e3790c5b0bd8c6b02ea3eaf5364c9a23bacbd791
Dede Iskandar <d3215k@gmail.com>
2023-12-21 21:30:42 +0700

    wip

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -55,6 +55,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->viteTheme('resources/css/filament/app/theme.css');
     }
 }
