9d7cf0e976d949137dc92fc819682b9d24af2a19
Dede Iskandar <d3215k@gmail.com>
2024-04-25 11:28:20 +0700

    Update pertanyaan-tertulis-pilihan-ganda-page.blade.php

--- resources/views/filament/resources/asesmen-resource/pages/pertanyaan-tertulis-pilihan-ganda-page.blade.php
+++ resources/views/filament/resources/asesmen-resource/pages/pertanyaan-tertulis-pilihan-ganda-page.blade.php
@@ -136,7 +136,7 @@
                                                                     wire:loading.attr="disabled"
                                                                     value="{{ $pilihanJawaban->id }}"
                                                                     disabled
-                                                                    @checked(isset($data['jawaban'][$pertanyaan->id]) && $data['jawaban'][$pertanyaan->id] === $pilihanJawaban->id)
+                                                                    @if (isset($data['jawaban'][$pertanyaan->id]) && $data['jawaban'][$pertanyaan->id] === $pilihanJawaban->id) checked @endif
                                                                     class="h-4 w-4 transition duration-75 bg-white border-none shadow-sm fi-radio-input ring-1 checked:ring-0 focus:ring-2 focus:ring-offset-0 disabled:bg-gray-50 disabled:text-gray-50 disabled:checked:bg-current disabled:checked:text-gray-400 dark:bg-white/5 dark:disabled:bg-transparent dark:disabled:checked:bg-gray-600 text-primary-600 ring-gray-950/10 focus:ring-primary-600 checked:focus:ring-primary-500/50 dark:text-primary-500 dark:ring-white/20 dark:checked:bg-primary-500 dark:focus:ring-primary-500 dark:checked:focus:ring-primary-400/50 dark:disabled:ring-white/10"
                                                                 />
                                                                 <label for="pilihan-jawaban.{{ $pilihanJawaban->id }}" class="block max-w-screen-xs prose dark:prose-invert bg-white dark:bg-gray-900">
